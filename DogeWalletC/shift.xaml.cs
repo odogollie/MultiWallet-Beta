0125ba59732c146a1f1ed0e626d0c3f9ce0a7f71
Oliver Shanklin <odogollie@gmail.com>
2014-11-27 21:17:17 -0700

    ListPicker Fixed on every page

--- DogeWalletC/shift.xaml.cs
+++ DogeWalletC/shift.xaml.cs
@@ -13,17 +13,16 @@ namespace DogeWalletC
 {
     public partial class shift : PhoneApplicationPage
     {
+        String[] currencys = {"Dogecoin", 
+                                "Bitcoin", 
+                                "Litecoin"};
+
         public shift()
         {
             InitializeComponent();
-            //this.SendCurrency.ItemsSource = currency;
-            //this.ReceiveCurrency.ItemsSource = currency;
+            this.SendCurrency.ItemsSource = currencys;
+            this.ReceiveCurrency.ItemsSource = currencys;
         }
-
-
-        
-
-        string[] currency = {"Dogecoin", "Bitcoin", "Litecoin"};
         string sendCurrency = "DogecoinAPIKey";
         string receiveCurrency = "BitcoinAPIKey";
 
@@ -33,9 +32,9 @@ namespace DogeWalletC
 
             if (ReceiveCurrency == null)
                 Read_API(receiveCurrency);
-            else if (ReceiveCurrency.SelectedIndex.Equals(1))
-                receiveCurrency = "BitcoinAPIKey";
             else if (ReceiveCurrency.SelectedIndex.Equals(0))
+                receiveCurrency = "BitcoinAPIKey";
+            else if (ReceiveCurrency.SelectedIndex.Equals(1))
                 receiveCurrency = "DogecoinAPIKey";
             else if (ReceiveCurrency.SelectedIndex.Equals(2))
                 receiveCurrency = "LitecoinAPIKey";
@@ -60,11 +59,45 @@ namespace DogeWalletC
         {
             if (IsolatedStorageSettings.ApplicationSettings.Contains(PickedCurrency))
             {
-                var apiKey = IsolatedStorageSettings.ApplicationSettings[PickedCurrency].ToString();
-                return apiKey;
+                return IsolatedStorageSettings.ApplicationSettings[PickedCurrency].ToString();
             }
             return "NoAPI";
 
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            PinBox.Text = string.Empty;
+            AmountBox.Text = string.Empty;
+        }
+        private void Shift_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (sendCurrency == receiveCurrency)
+            {
+                DisplayMessage("Please make sure the currencies are not the same!");
+            }
+            else
+            {
+                var sendAPI = Read_API(sendCurrency);
+                var receiveAPI = Read_API(receiveCurrency);
+                var amount = AmountBox.Text;
+                double amountD = Double.Parse(AmountBox.Text);
+                var pin = PinBox.Text;
+
+                if (amount == string.Empty || amountD == 0)
+                {
+                    DisplayMessage("Please set an amount to receive!");
+                }
+                else if (pin.Equals(string.Empty))
+                {
+                    DisplayMessage("Please Type in Pin!");
+                }
+            }
+
+        }
+        private void DisplayMessage(string error)
+        {
+            MessageBoxResult messageBox = MessageBox.Show(error, "Error!", MessageBoxButton.OK);
         }
     }
 }
