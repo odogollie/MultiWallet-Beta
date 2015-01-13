4c9e3136956ae165be6f4a134024cab7dfc9f6a4
Oliver Shanklin <odogollie@gmail.com>
2015-01-13 15:46:07 -0700

    v2 pages

---
+++ MultiWallet v2/MultiWallet/MultiWallet/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MultiWallet.Resources;
+using System.IO.IsolatedStorage;
+
+namespace MultiWallet
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        const string settingsAppLaunched = "appLaunched";
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Check to see if First Run
+
+            /*if (IsFirstLaunch())
+            {
+                global_methods.DisplayMessage("Please Set API Keys", "First Launch");
+                Launched();
+
+                // Send to first_run.xaml
+                NavigationService.Navigate(new Uri("/first_run.xaml", UriKind.Absolute));
+            }
+            else
+            {
+                //string navTo = "home.xaml";
+                //NavigationService.Navigate(new Uri(navTo, UriKind.RelativeOrAbsolute));
+                //GoHome();
+            } */
+
+        }
+
+        private static bool IsFirstLaunch()
+        {
+            IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+            
+            return !(settings.Contains(settingsAppLaunched));
+        }
+
+        private static void Launched()
+        {
+            if (IsFirstLaunch())
+            {
+                IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+                settings.Add(settingsAppLaunched, true);
+                settings.Save();
+            }
+        }
+
+        private void DisplayMessage(string message, string title)
+        {
+            MessageBoxResult messageBox = MessageBox.Show(message, title, MessageBoxButton.OK);
+        }
+
+        private void NavTo(string uri)
+        {
+            NavigationService.Navigate(new Uri(uri, UriKind.RelativeOrAbsolute));
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadingBar.Visibility = Visibility.Collapsed;
+
+            /*if (IsFirstLaunch())
+            {
+                //global_methods.DisplayMessage("Please Set API Keys", "First Launch");
+                Launched();
+
+                // Send to first_run.xaml
+                //NavigationService.Navigate(new Uri("/first_run.xaml", UriKind.Absolute));
+                NavTo("/first_run.xaml");
+            }
+            else
+            {
+                //string navTo = "home.xaml";
+                //NavigationService.Navigate(new Uri(navTo, UriKind.RelativeOrAbsolute));
+                NavTo("/home.xaml");
+            }*/
+        }
+            
+
+        
+    }
+}
---
+++ MultiWallet v2/MultiWallet/MultiWallet/global_methods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MultiWallet
+{
+    class global_methods
+    {
+
+        // Display Message with 2 args
+        internal static void DisplayMessage(string message, string title)
+        {
+            MessageBoxResult messageBox = MessageBox.Show(message, title, MessageBoxButton.OK);
+        }
+
+        // Read API Keys
+        internal static string ReadAPIKey(string network)
+        {
+            // Read isolated storage for api key for specific network
+            return "";
+        }
+
+        // Save API Keys
+        internal static void SaveAPIKey(string network, string apikey)
+        {
+            // Save keys in isolated storage
+
+        }
+
+        // Get Default Currency
+        internal static string GetDefaultCurrency()
+        {
+            // Read isolated storage value for default currency
+            return "";
+        }
+
+        // Set Default Currency
+        internal static void SetDefaultCurrency()
+        {
+            // Set isolated storage value of default currency
+        }
+
+
+    }
+
+
+}
